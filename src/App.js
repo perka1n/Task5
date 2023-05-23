55d8b7407d884d6338479c551d556c47270d9154
Danil <flowerdaner@gmail.com>
2023-05-23 12:30:27 +0400

    added file MyPages wheres func for pages

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import { MyPages } from "./MyPages";
 
 function App() {
   return (
+      <Router>
       <>
       <nav>
-      <link to='/'>Статьи</link>
-      <link to='/tests'>Тесты</link>
-      <link to='/games'>Игры</link>
+        <link to='/'>Статьи</link>
+        <link to='/tests'>Тесты</link>
+        <link to='/games'>Игры</link>
       </nav>
+
+      <Switch>
+          <Route path='/tests'>
+            <About />
+          </Route>
+          <Route path='/games'>
+            <Contact />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+      </Switch>
       </>
+      </Router>
   );
 }
 
